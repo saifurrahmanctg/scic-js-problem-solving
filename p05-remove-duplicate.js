f742b044329baeea6afd50bca6815cfbd0014aac
saifurrahmanctg <chysaifurrahmanbd@gmail.com>
2026-01-08 00:57:32 +0600

    SCIC 12 - Assignmment 07 - JS Problem Solving 05 - Remove Duplicates from an Array

---
+++ p05-remove-duplicate.js
@@ -0,0 +1,12 @@
+function removeDuplicates(arr) {
+  let unique = [];
+
+  for (let num of arr) {
+    if (!unique.includes(num)) {
+      unique.push(num);
+    }
+  }
+  return unique;
+}
+
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4]));
