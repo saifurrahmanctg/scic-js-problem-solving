28affabb1bbe9857e14c050c70d28027ee5e048d
saifurrahmanctg <chysaifurrahmanbd@gmail.com>
2026-01-08 01:03:27 +0600

    SCIC 12 - Assignmment 07 - JS Problem Solving 07 - Find Even Numbers in an Array

---
+++ p07-find-even-num.js
@@ -0,0 +1,10 @@
+function findEvenNumbers(arr) {
+  let evens = [];
+  for (let num of arr) {
+    if (num % 2 === 0) {
+      evens.push(num);
+    }
+  }
+  return evens;
+}
+console.log(findEvenNumbers([1, 2, 3, 4, 5, 6]));
