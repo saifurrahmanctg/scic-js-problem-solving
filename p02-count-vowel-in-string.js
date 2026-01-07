5355bdad218d91be6ac53005ac44f7808f1a9eb0
saifurrahmanctg <chysaifurrahmanbd@gmail.com>
2026-01-08 00:42:12 +0600

    SCIC 12 - Assignmment 07 - JS Problem Solving 02 - Count Vowels in a String

---
+++ p02-count-vowel-in-string.js
@@ -0,0 +1,11 @@
+function countVowels(str) {
+  const vowels = "aeiouAEIOU";
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countVowels("programming"));
