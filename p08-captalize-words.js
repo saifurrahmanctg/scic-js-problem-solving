15d62550df24c3b909ca0b11fd443130fb8657cb
saifurrahmanctg <chysaifurrahmanbd@gmail.com>
2026-01-08 01:06:46 +0600

    SCIC 12 - Assignmment 07 - JS Problem Solving 08 - Capitalize First Letter of Each Word

---
+++ p08-captalize-words.js
@@ -0,0 +1,7 @@
+function capitalizeWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(" ");
+}
+console.log(capitalizeWords("hello world"));
